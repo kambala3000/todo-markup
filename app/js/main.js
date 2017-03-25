e9ab0406a4471e36aff74bffad43b9d4809b4ef6
kambala3000 <kambala02@gmail.com>
2017-03-25 22:33:50 +0200

    1024px-ver, started dev mobile ver

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,20 @@
 "use strict";
+
+var viewportInfo = {
+    get width() {
+        return $(window).width();
+    },
+    get isMobile() {
+        return this.width < 1025;
+    },
+    get isSmallscreen() {
+        return this.width > 1024 && this.width < 1441;
+    },
+    get isWidescreen() {
+        return this.width > 1440;
+    }
+};
+
+$(function () {
+    $(window).resize(function () {});
+});
